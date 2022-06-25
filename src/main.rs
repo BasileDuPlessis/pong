1396dda2fda2f3cb5ad3292cd9a4bc463fbfec5a
basile du Plessis <basile.duplessis@gmail.com>
2022-06-25 11:58:30 +0200

    Bounce against vertical and horizontal wall

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,34 @@ struct Ball {
 }
 
 impl Ball {
-    //turn ball direction 180°
-    fn bounce(&mut self, wall_angle:f32) {
-        let wall_orthogonal_angle = wall_angle + 90.;
-        let orthogonal_bounce_angle = self.a + 180.;
-
-        self.a = 2. * wall_orthogonal_angle - orthogonal_bounce_angle;
-
+    //make ball bounce against the right wall 
+    fn bounce_right(&mut self) {
+        if self.a > 270. {
+            self.a = 360. + 180. - self.a;
+        } else {
+            self.a = 180. - self.a;
+        }
     }
+
+     //make ball bounce against the left wall 
+     fn bounce_left(&mut self) {
+        if self.a < 180. {
+            self.a = 180. - self.a;
+        } else {
+            self.a = 360. + 180. - self.a;
+        }
+    }
+
+    //make ball bounce against the ceil 
+    fn bounce_ceil(&mut self) {
+        self.a = 360. - self.a;
+    }
+
+    //make ball bounce against the ceil 
+    fn bounce_floor(&mut self) {
+        self.a = 360. - self.a;
+    }
+
     //move ball one step ahead
     fn step(&mut self) {
         self.w += (self.a.cos() * self.s) as i16;
@@ -29,13 +49,53 @@ impl Ball {
 
 
 #[test]
-//Test wether bounce effectively change direction of the ball like a wall do
-fn test_bounce_effective_rotation() {
-    let mut ball = Ball { w: 0, h: 0, a: 30., s:2. };
-    
-    ball.bounce(110.);    
-    assert_eq!(ball.a, 190.);
+//Test bounce from RIGHT to LEFT
+fn test_bounce_right_to_left() {
+    let mut ball = Ball { w: 0, h: 0, a: 340., s:2. };    
+    ball.bounce_right();    
+    assert_eq!(ball.a, 200.);
+
+    ball.a = 20.;
+    ball.bounce_right();    
+    assert_eq!(ball.a, 160.);
 }
+
+#[test]
+//Test bounce from LEFT to RIGHT
+fn test_bounce_left_to_right() {
+    let mut ball = Ball { w: 0, h: 0, a: 160., s:2. };    
+    ball.bounce_left();    
+    assert_eq!(ball.a, 20.);
+
+    ball.a = 200.;
+    ball.bounce_left();    
+    assert_eq!(ball.a, 340.);
+}
+
+#[test]
+//Test bounce on ceil
+fn test_bounce_ceil() {
+    let mut ball = Ball { w: 0, h: 0, a: 20., s:2. };    
+    ball.bounce_ceil();    
+    assert_eq!(ball.a, 340.);
+
+    ball.a = 160.;
+    ball.bounce_ceil();    
+    assert_eq!(ball.a, 200.);
+}
+
+#[test]
+//Test bounce on floor
+fn test_bounce_floor() {
+    let mut ball = Ball { w: 0, h: 0, a: 340., s:2. };    
+    ball.bounce_floor();    
+    assert_eq!(ball.a, 20.);
+
+    ball.a = 200.;
+    ball.bounce_floor();    
+    assert_eq!(ball.a, 160.);
+}
+
 
 
 #[test]
